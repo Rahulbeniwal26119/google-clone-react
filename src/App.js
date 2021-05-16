c407753743b9d6a2c1d8ad77b03c62e5521b63ff
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-16 17:38:00 +0530

    adding Route for /search

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/pages/Home';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import SearchPage from "./components/pages/SearchPage";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/search">
-            <h1>This is a search page </h1>
-            </Route>
+            <SearchPage/>
+          </Route>
         </Switch>
       </Router>
-      Hello 
     </div>
   );
 }
