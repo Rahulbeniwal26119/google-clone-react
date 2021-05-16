984cba06d0a3698920c2d0305dde744336c72394
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-16 17:32:39 +0530

    State Provider

---
+++ src/StateProvider.js
@@ -0,0 +1,12 @@
+import React, {createContext, useContext, useReducer} from "react"
+
+const StateContext = createContext();  // data layer 
+
+export const StateProvider= ({reducer, initialState, children})=> (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+)
+
+export const useStateValue = () => useContext(StateContext)
+
