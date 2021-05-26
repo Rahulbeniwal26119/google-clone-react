24e96482f7044c598c4bd5171155dd8a1216e550
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-05-26 12:39:37 +0530

    adding search page for search term

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import "./SearchPage.css";
 import {useStateValue} from "../../StateProvider"
+import useGoogleSearch from './useGoogleSearch';
+import Search from './Search';
 function SearchPage() {
     const [{term}, dispatch] = useStateValue();
+    // const {data} = useGoogleSearch(term)
+
+    // console.log(data);
     return (
         <div className="searchPage">
             <div className="searchPage_header">
-                <h1>On search page </h1>
+                <Link to="/">
+                <img className="googleSearchLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png"/>
+                </Link>
+                <div className="searchPageHeaderBody">
+                <Search hideButtons/>
+                </div>
                 <h1>{term}</h1>
             </div>
             <div className="searchPage_result">
-
+                
             </div>
         </div>
     )
